1db3f8724496907b92b525857ad5c79fc2375ed0
Pierre Charles <40791587+Pierre-Charles@users.noreply.github.com>
2019-11-01 23:43:39 +0000

    Update asyncserver.ino

--- asyncserver/asyncserver.ino
+++ asyncserver/asyncserver.ino
@@ -2,9 +2,6 @@
 #include "WiFi.h"
 #include "ESPAsyncWebServer.h"
 
-const char* ssid = "VM3156656";
-const char* password = "jn3qvGncNbn8";
-
 const int ldr = 34; // Pin used for LDR sensor
 const int sw420 = 35;
 int light_value = 0;
