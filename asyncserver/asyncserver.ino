8fd787c99a4a67d4ce0e073126b36d57be63a1bf
pierre-charles <p.charles0501@gmail.com>
2019-11-01 01:52:47 +0000

    Re-designing web server front end

--- asyncserver/asyncserver.ino
+++ asyncserver/asyncserver.ino
@@ -2,50 +2,114 @@
 #include "WiFi.h"
 #include "ESPAsyncWebServer.h"
 
+const char* ssid = "Vladimir_Routin";
+const char* password = "Ad%eliePen%guin5";
+
 const int ldr = 34; // Pin used for LDR sensor
+const int sw420 = 35;
 int light_value = 0;
 
 AsyncWebServer server(80); // AsyncWebServer object on port 80
 
 String readLDR() {
   light_value = analogRead(ldr);
+  Serial.println("Reading from DR: ");
   Serial.println(light_value);
   return String(light_value);
 }
 
+String readSW420() {
+  long measurement = pulseIn(sw420, HIGH);
+  Serial.println("Reading from SW-420");
+  Serial.println(measurement);
+  return String(measurement);
+}
+
 const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
+<!DOCTYPE HTML>
+<html>
+
 <head>
   <title>TumbleBot</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
   <style>
-  @import url('https://fonts.googleapis.com/css?family=Fira+Mono|Lato|Montserrat|Open+Sans|Ubuntu|Ubuntu+Mono&display=swap');
+    @import url('https://fonts.googleapis.com/css?family=Fira+Mono|Lato|Roboto|Montserrat|Open+Sans|Ubuntu|Ubuntu+Mono&display=swap');
+
     html {
-     font-family: Open Sans, sans-serif;
-     display: inline-block;
-     margin: 0px auto;
-     text-align: center;
+      font-family: 'Roboto';
+      display: inline-block;
+      margin: 0px auto;
+      text-align: center;
     }
-    h2 { font-size: 3.0rem; font-family: Ubuntu Mono, monospace; color: #5455b3; }
-    p { font-size: 3.0rem; }
-    .units { font-size: 1.2rem; }
-    .dht-labels{
+
+    body {
+      background: url(https://hdqwalls.com/wallpapers/blur-background-6z.jpg) no-repeat center center fixed;
+    }
+
+    .dht-labels {
       font-size: 1.5rem;
-      vertical-align:middle;
+      vertical-align: middle;
       padding-bottom: 15px;
+    }
+
+    .readings {
+      font-size: 1.5rem;
+      vertical-align: middle;
+      padding-bottom: 15px;
+      color: purple
+    }
+
+    .container {
+      font-family: 'Roboto';
+      width: 600px;
+      margin: 70px auto 0;
+      display: block;
+      background: #ececec;
+      padding: 10px 50px 50px;
+      border: 5px solid #ebebeb;
+      box-shadow: 0 19px 38px rgba(0, 0, 0, 0.30), 0 15px 12px rgba(0, 0, 0, 0.22);
+    }
+
+    h2 {
+      text-align: center;
+      margin-bottom: 50px;
+    }
+
+    h2 small {
+      padding-top: 10px;
+      font-weight: normal;
+      color: #888;
+      display: block;
+    }
+
+    .footer {
+      padding-top: 100px;
+      text-align: center;
     }
   </style>
 </head>
+
 <body>
-  <h2>TumbleBot</h2>
-  <h4>Tumbledryer Monitoring System</h4>
-  <p>
-    <i class="fas fa-microchip" style="color:#581845;"></i> 
-    <span class="dht-labels">LDR Reading</span> 
-    <span id="ldr">%LDR%</span>
-  </p>
+  <div class="container">
+    <h2>TumbleBot<small>Tumbledryer Monitoring System</small></h2>
+    <br>
+    <p>
+      <i class="fas fa-microchip" style="color:#581845;"></i>
+      <span class="dht-labels">LDR Reading: </span>
+      <span class="readings" id="ldr">%LDR%</span>
+    </p>
+    <p>
+      <i class="fas fa-memory" style="color:#581845;"></i>
+      <span class="dht-labels">SW420 Reading: </span>
+      <span class="readings" id="sw420">%SW420%</span>
+      </span>
+      <br>
+      <p class="footer">An IoT device by Pierre Charles</p>
+  </div>
 </body>
+
 <script>
 setInterval(function ( ) {
   var xhttp = new XMLHttpRequest();
@@ -56,7 +120,18 @@ setInterval(function ( ) {
   };
   xhttp.open("GET", "/ldr", true);
   xhttp.send();
-}, 1000 ) ;
+}, 1000);
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("sw420").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/sw420", true);
+  xhttp.send();
+}, 1000) ;
 </script>
 </html>)rawliteral";
 
@@ -65,8 +140,9 @@ String processor(const String& var) {
   //Serial.println(var);
   if (var == "LDR") {
     return readLDR();
+  } else if (var == "SW420") {
+    return readSW420();
   }
-
   return String();
 }
 
@@ -74,6 +150,7 @@ void setup() {
   // Serial port for debugging purposes
   Serial.begin(115200);
   pinMode(ldr, INPUT);
+  pinMode(sw420, INPUT);
 
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
@@ -91,6 +168,9 @@ void setup() {
   });
   server.on("/ldr", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send_P(200, "text/plain", readLDR().c_str());
+  });
+  server.on("/sw420", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send_P(200, "text/plain", readSW420().c_str());
   });
 
   // Start server
