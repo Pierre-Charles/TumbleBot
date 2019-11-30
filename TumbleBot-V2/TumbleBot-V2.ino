c236c122d238a20bf5512f3bc09d07fe3c262f11
Pierre Charles <40791587+Pierre-Charles@users.noreply.github.com>
2019-11-30 14:21:18 +0000

    Changing UUID formatting

--- TumbleBot-V2/TumbleBot-V2.ino
+++ TumbleBot-V2/TumbleBot-V2.ino
@@ -298,9 +298,9 @@ void readRFID() {
   String content = "";
   byte letter;
   for (byte i = 0; i < mfrc522.uid.size; i++) {
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? "0" : " ");
     Serial.print(mfrc522.uid.uidByte[i], HEX);
-    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : " "));
     content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
 
